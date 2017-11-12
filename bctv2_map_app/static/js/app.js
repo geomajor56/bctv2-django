96235748cef00615c932b875fdbac9a24960b92f
michael <geomajor56@gmail.com>
2017-11-12 08:51:50 -0500

    tinymce leaflet

--- bctv2_map_app/static/js/app.js
+++ bctv2_map_app/static/js/app.js
@@ -1,53 +1,54 @@
 var map, featureList, pointSearch = [], tour;
 
-L.MakiMarkers.accessToken = "pk.eyJ1IjoiZ2VvbWFqb3I1NiIsImEiOiJjaW9iejZ4cGYwNDc0dnpsejBmc2g0Z3QzIn0.8hKDWYbdQW7cbIE7eeu4-A";
 
-$(document).ready(function () {
+    L.MakiMarkers.accessToken = "pk.eyJ1IjoiZ2VvbWFqb3I1NiIsImEiOiJjaW9iejZ4cGYwNDc0dnpsejBmc2g0Z3QzIn0.8hKDWYbdQW7cbIE7eeu4-A";
 
-    console.log('Hey MotherFuckers!!!')
+    $(document).ready(function () {
 
-    $("#welcome").modal("show");
-    $(window).resize(function () {
-        sizeLayerControl();
-    });
+        console.log('Hey MotherFuckers!!!')
 
-    $(document).on("click", ".feature-row", function (e) {
-        $(document).off("mouseout", ".feature-row", clearHighlight);
-        sidebarClick(parseInt($(this).attr("id"), 10));
-    });
-
-    if (!("ontouchstart" in window)) {
-        $(document).on("mouseover", ".feature-row", function (e) {
-            highlight.clearLayers().addLayer(L.circleMarker([$(this).attr("lat"), $(this).attr("lng")], highlightStyle));
+        $("#welcome").modal("show");
+        $(window).resize(function () {
+            sizeLayerControl();
         });
-    }
 
-    // $(document).on("mouseout", ".feature-row", clearHighlight);
-
-
-    $("#legend-btn").click(function () {
-        $("#legendModal").modal("show");
-        $(".navbar-collapse.in").collapse("hide");
-        return false;
-    });
-
-    $("#slideshow-btn").click(function () {
-        jQuery("#bct_nanogallery2").nanogallery2({
-            userID: '66767545@N02',
-            kind: 'flickr',
-            // displayBreadcrumb: 'false',
-            thumbnailWidth: 150,
-            thumbnailHeight: 150,
-            colorScheme: 'dark',
-            galleryDisplayMode: "pagination",
-            galleryMaxRows: 2,
-            galleryPaginationMode: "numbers",
-            album: 'none',
-
-
+        $(document).on("click", ".feature-row", function (e) {
+            $(document).off("mouseout", ".feature-row", clearHighlight);
+            sidebarClick(parseInt($(this).attr("id"), 10));
         });
-        $("#slideshowModal").modal("show");
-        $(".navbar-collapse.in").collapse("hide");
-        return false;
+
+        if (!("ontouchstart" in window)) {
+            $(document).on("mouseover", ".feature-row", function (e) {
+                highlight.clearLayers().addLayer(L.circleMarker([$(this).attr("lat"), $(this).attr("lng")], highlightStyle));
+            });
+        }
+
+        // $(document).on("mouseout", ".feature-row", clearHighlight);
+
+
+        $("#legend-btn").click(function () {
+            $("#legendModal").modal("show");
+            $(".navbar-collapse.in").collapse("hide");
+            return false;
+        });
+
+        $("#slideshow-btn").click(function () {
+            jQuery("#bct_nanogallery2").nanogallery2({
+                userID: '66767545@N02',
+                kind: 'flickr',
+                // displayBreadcrumb: 'false',
+                thumbnailWidth: 150,
+                thumbnailHeight: 150,
+                colorScheme: 'dark',
+                galleryDisplayMode: "pagination",
+                galleryMaxRows: 2,
+                galleryPaginationMode: "numbers",
+                album: 'none',
+
+
+            });
+            $("#slideshowModal").modal("show");
+            $(".navbar-collapse.in").collapse("hide");
+            return false;
+        });
     });
-});

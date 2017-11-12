a9422de5f0102efac55888341a1430e5df402554
michael <geomajor56@gmail.com>
2017-11-12 08:53:07 -0500

    tinymce leaflet

--- bctv2_map_app/views.py
+++ bctv2_map_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from bctv2_map_app.models import Narrative
+from django.views import View
+from django.views.generic import TemplateView, ListView
 
-# Create your views here.
+
+def intro_narrative(request):
+    welcomepage = Narrative.objects.all()
+    return render(request, 'base.html', {'welcomepage': welcomepage})
+
+
+class ModalView(View):
+    def get(self, request, *args, **kwargs):
+        welcomepage = Narrative.objects.all()
+        return render(request, 'base.html', {'welcomepage': welcomepage})
+
+
+
+
+
+class ModalTemplateView(TemplateView):
+    template_name = 'base.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ModalTemplateView, self).get_context_data(*args, **kwargs)
+        welcomepage = Narrative.objects.all()
+        # {context: welcomepage}
+
+        return context
